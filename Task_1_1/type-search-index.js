f0a25953026c54df8bba0d4ec881f9d2237a9c27
Repeynik <Repeynik@users.noreply.github.com>
2024-09-16 10:29:26 +0000

    Deploying to gh-pages from @ Repeynik/OOP@652b99cb1118e7a9b37b2aa0294b4bb4bae0c621 🚀

--- Task_1_1/type-search-index.js
+++ Task_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.task_1","l":"HeapSort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.task_1","l":"HeapSort"},{"p":"org.task_1","l":"Main"}];updateSearchResults();
