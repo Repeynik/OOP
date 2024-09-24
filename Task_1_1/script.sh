64e8d025c30a9954968ba6f6f92ec0a2239697f7
Alena Savushkina <a.savushkina@g.nsu.ru>
2024-09-24 13:08:22 +0700

    script fix

--- Task_1_1/script.sh
+++ Task_1_1/script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-SRC_DIR="app/src/main/java/org/task_1"
+SRC_DIR="OOP/Task_1_1/app/src/main/java/org/task_1"
 BIN_DIR="app/bin"
 DOC_DIR="build/docs/javadoc"
 MAIN_CLASS="org.task_1.Main"
 
 echo "Компиляция исходников..."
-javac -d "$BIN_DIR" "$SRC_DIR/Main.java" "$SRC_DIR/HeapSort.java" "$SRC_DIR/ArrayHelper.java" "$SRC_DIR/PerformanceTest.java"
+javac -d "$BIN_DIR" "$SRC_DIR/Main.java" "$SRC_DIR/HeapSort.java" "$SRC_DIR/ArrayHelper.java"
 
 echo "Генерация документации..."
-javadoc -d "$DOC_DIR" "$SRC_DIR/Main.java" "$SRC_DIR/HeapSort.java" "$SRC_DIR/ArrayHelper.java" "$SRC_DIR/PerformanceTest.java"
+javadoc -d "$DOC_DIR" "$SRC_DIR/Main.java" "$SRC_DIR/HeapSort.java" "$SRC_DIR/ArrayHelper.java"
 
 echo "Создание jar файла..."
-jar cf myApp.jar -C "$BIN_DIR" .
+jar cf --main-class="$MAIN_CLASS" myApp.jar -C "$BIN_DIR" .
 
 echo "Запуск приложения..."
 java -jar myApp.jar
